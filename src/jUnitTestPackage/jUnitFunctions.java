15520bb3765977b525a97a33a298de200951330e
Arnav Kumar Mittal <arnavmittal17@gmail.com>
2022-11-18 16:31:59 +0530

    Adding

---
+++ src/jUnitTestPackage/jUnitFunctions.java
@@ -0,0 +1,13 @@
+package jUnitTestPackage;
+
+public class jUnitFunctions {
+	public int addNumbers(int n1, int n2) {
+		int ans = n1 + n2;
+		return ans;
+	}
+	public String addStrings(String s1, String s2) {
+		String ans1 = s1 + s2;
+		return ans1;
+	}
+
+}
